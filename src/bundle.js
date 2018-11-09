c447ca7011306a8ef386650b1706263ebd51609a
Maxim Pavlov <ifelseapps@gmail.com>
2018-11-10 00:54:20 +0300

    Контент, переключение тем

--- src/bundle.js
+++ src/bundle.js
@@ -1,1 +1,18 @@
 import './styles.css';
+
+const customPropertiesSupports = window.CSS && window.CSS.supports('--test-property', 0);
+const themeToggler = document.querySelector('.js-theme-toggler');
+const themeMeta = document.querySelector('meta[name="theme-color"]');
+const defaultTheme = themeMeta.content;
+
+themeToggler.addEventListener('change', function () {
+  document.documentElement.classList.toggle('theme-dark');
+
+  const blackTheme = customPropertiesSupports
+    ? getComputedStyle(document.documentElement).getPropertyValue('--theme-background')
+    : '';
+
+  themeMeta.content = this.checked ? blackTheme : defaultTheme;
+
+  // TODO: сохранять значение в LocalStorage
+});
