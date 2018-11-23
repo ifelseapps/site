8cd3b42f79d29fc28f0993dad82c49f85d7d08f9
Maxim Pavlov <ifelseapps@gmail.com>
2018-11-24 00:13:59 +0300

    Доработка переключателя тем

--- src/bundle.js
+++ src/bundle.js
@@ -2,10 +2,23 @@ import './bundle.css';
 
 const customPropertiesSupports = window.CSS && window.CSS.supports('--test-property', 0);
 const themeToggler = document.querySelector('.js-theme-toggler');
+const themeTogglerCheckbox = themeToggler.querySelector('.js-checkbox');
 const themeMeta = document.querySelector('meta[name="theme-color"]');
 const defaultTheme = themeMeta.content;
+const LOCAL_STORAGE_KEY = 'dark_theme';
+const isDarkThemeEnabled = localStorage[LOCAL_STORAGE_KEY] === 'Y';
 
-themeToggler.addEventListener('change', function () {
+document.addEventListener('DOMContentLoaded', function () {
+  if (window.localStorage && isDarkThemeEnabled) {
+    document.documentElement.classList.add('theme-dark');
+    themeTogglerCheckbox.checked = true;
+  }
+
+  // Включаем transition'ы после того как передвинули toggler в нужное положение
+  setTimeout(() => themeToggler.classList.remove('theme-switcher--animation-off'), 0);
+});
+
+themeTogglerCheckbox.addEventListener('change', function () {
   document.documentElement.classList.toggle('theme-dark');
 
   const blackTheme = customPropertiesSupports
@@ -14,5 +27,7 @@ themeToggler.addEventListener('change', function () {
 
   themeMeta.content = this.checked ? blackTheme : defaultTheme;
 
-  // TODO: сохранять значение в LocalStorage
+  if (window.localStorage) {
+    localStorage[LOCAL_STORAGE_KEY] = this.checked ? 'Y' : 'N';
+  }
 });
