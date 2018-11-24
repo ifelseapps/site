362d990b14ca71094fc1c7f4e3154e37bdaf937e
Maxim Pavlov <ifelseapps@gmail.com>
2018-11-24 08:11:30 +0300

    Фикс для переключателя тем

--- src/bundle.js
+++ src/bundle.js
@@ -8,15 +8,7 @@ const defaultTheme = themeMeta.content;
 const LOCAL_STORAGE_KEY = 'dark_theme';
 const isDarkThemeEnabled = localStorage[LOCAL_STORAGE_KEY] === 'Y';
 
-document.addEventListener('DOMContentLoaded', function () {
-  if (window.localStorage && isDarkThemeEnabled) {
-    document.documentElement.classList.add('theme-dark');
-    themeTogglerCheckbox.checked = true;
-  }
-
-  // Включаем transition'ы после того как передвинули toggler в нужное положение
-  setTimeout(() => themeToggler.classList.remove('theme-switcher--animation-off'), 0);
-});
+// TODO: создать themeManager для удобного управления темами
 
 themeTogglerCheckbox.addEventListener('change', function () {
   document.documentElement.classList.toggle('theme-dark');
@@ -33,4 +25,13 @@ themeTogglerCheckbox.addEventListener('change', function () {
   if (window.localStorage) {
     localStorage[LOCAL_STORAGE_KEY] = this.checked ? 'Y' : 'N';
   }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  if (window.localStorage && isDarkThemeEnabled) {
+    themeTogglerCheckbox.click();
+  }
+
+  // Включаем transition'ы после того как передвинули toggler в нужное положение
+  setTimeout(() => themeToggler.classList.remove('theme-switcher--animation-off'), 0);
 });
